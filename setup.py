b15a79f9f21b78df4a96af777b263c9f9730e2a1
Lavanya Gupta <lavanyagupta294@gmail.com>
2025-06-27 15:48:36 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot Project',
+    version= '0.0.0',
+    author= 'Lavanya Gupta',
+    author_email= 'lavanyagupta294@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
